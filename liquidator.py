285a801b48200248d44e9e3ec3852d7ebab69377
Freeeedy <hadraba87@gmail.com>
2025-10-29 14:46:59 +0100

    mhmm

--- liquidator.py
+++ liquidator.py
@@ -1,5 +1,8 @@
 import subprocess
 import os
+import platform
+
+os_name = platform.system()
 
 # DOCUMENTS
 doc_extensions = [
@@ -49,21 +52,45 @@ file = input("what file would you like to liquidate?\n")
 
 ext = os.path.splitext(file)[1]  # gives you ".txt"
 ext = ext.replace('.', '')       # remove the dot
+if os_name == "Linux":
+    if ext in doc_extensions + image_extensions + design_extensions:
+        subprocess.run(f'mat2 --inplace "{file}"', shell=True)
+        subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
+        subprocess.run(f'touch -t 200001010000 "{file}"', shell=True)  
 
-if ext in doc_extensions + image_extensions + design_extensions:
-    subprocess.run(f'mat2 --inplace "{file}"', shell=True)
-    subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
-    subprocess.run(f'touch -t 200001010000 "{file}"', shell=True)  
-elif ext in text_extensions:
-    subprocess.run(f'cp "{file}" newfile.{ext}', shell=True)
-    subprocess.run(f'mv newfile.{ext} "{file}"', shell=True)
-    subprocess.run(f'touch -t 200001010000 "{file}"', shell=True) 
-elif ext in video_extensions + audio_extensions:
-    subprocess.run(f'ffmpeg -i "{file}" -map_metadata -1 -c:v copy -c:a copy newfile.{ext}', shell=True)
-    subprocess.run(f'mv newfile.{ext} "{file}"', shell=True)
-    subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
-    subprocess.run(f'touch -t 200001010000 "{file}"', shell=True)  
-if ext in ebook_extensions:
-    subprocess.run(f'mat2 --inplace "{file}"', shell=True)
-    subprocess.run(f'ebook-meta "{file}" --delete-metadata', shell=True)
-    subprocess.run(f'touch -t 200001010000 "{file}"', shell=True)  
+    elif ext in text_extensions:
+        subprocess.run(f'cp "{file}" newfile.{ext}', shell=True)
+        subprocess.run(f'mv newfile.{ext} "{file}"', shell=True)
+        subprocess.run(f'touch -t 200001010000 "{file}"', shell=True) 
+        
+    elif ext in video_extensions + audio_extensions:
+        subprocess.run(f'ffmpeg -i "{file}" -map_metadata -1 -c:v copy -c:a copy newfile.{ext}', shell=True)
+        subprocess.run(f'mv newfile.{ext} "{file}"', shell=True)
+        subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
+        subprocess.run(f'touch -t 200001010000 "{file}"', shell=True) 
+
+    if ext in ebook_extensions:
+        subprocess.run(f'mat2 --inplace "{file}"', shell=True)
+        subprocess.run(f'ebook-meta "{file}" --delete-metadata', shell=True)
+        subprocess.run(f'touch -t 200001010000 "{file}"', shell=True)
+
+elif os_name == "Windows":
+    if ext in doc_extensions + image_extensions + design_extensions:
+        subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
+        subprocess.run(f'powershell -Command "(Get-Item \'{file}\').LastWriteTime = \'2000-01-01T00:00:00\'"', shell=True)
+
+    elif ext in text_extensions:
+        subprocess.run(f'copy "{file}" newfile.{ext}', shell=True)
+        subprocess.run(f'move "newfile.{ext}" "{file}"', shell=True)
+        subprocess.run(f'powershell -Command "(Get-Item \\"{file}\\").LastWriteTime = (Get-Date \\"2000-01-01T00:00:00\\")"', shell=True)
+
+    elif ext in video_extensions + audio_extensions:
+        subprocess.run(f'ffmpeg -i "{file}" -map_metadata -1 -c:v copy -c:a copy newfile.{ext}', shell=True)
+        subprocess.run(f'move newfile.{ext} -Destination "{file}"', shell=True)
+        subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
+        subprocess.run(f'powershell -Command "(Get-Item \'{file}\').LastWriteTime = \'2000-01-01T00:00:00\'"', shell=True)
+
+    elif ext in ebook_extensions:
+        subprocess.run(f'exiftool -all= -overwrite_original "{file}"', shell=True)
+        subprocess.run(f'ebook-meta "{file}" --delete-metadata', shell=True)
+        subprocess.run(f'powershell -Command "(Get-Item \'{file}\').LastWriteTime = \'2000-01-01T00:00:00\'"', shell=True)
